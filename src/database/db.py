22fa291c2744725e53f8526c48a9a1c15ea4cb44
Olga Mageria <olgamageria@gmail.com>
2024-04-27 10:14:16 +0300

    Revert "settings + frontend"

--- src/database/db.py
+++ src/database/db.py
@@ -1,12 +1,9 @@
 import contextlib
-import logging
 
 from sqlalchemy.ext.asyncio import AsyncEngine, async_sessionmaker, create_async_engine
-import redis.asyncio as redis
 
 
 from src.config.config import config
-logger = logging.getLogger(f"{config.APP_NAME}.{__name__}")
 
 
 class DataBaseSessionManager:
@@ -31,49 +28,6 @@ class DataBaseSessionManager:
 sessionmanager = DataBaseSessionManager(config.DATABASE_URL)
 
 
-def get_db():
-    db = DBSession()
-    try:
-        yield db
-    except SQLAlchemyError as err:
-        print("SQLAlchemyError:", err)
-        db.rollback()
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(err))
-    finally:
-        db.close()
-
-
-def create_redis():
-    return redis.ConnectionPool(
-        host=config.REDIS_DOMAIN,
-        port=config.REDIS_PORT,
-        password=config.REDIS_PASSWORD,
-        db=0,
-        decode_responses=False,
-    )
-
-
-def get_redis() -> redis.Redis | None:
-    # Here, we re-use our connection pool
-    # not creating a new one
-    try:
-        logger.debug("get_redis connection_pool")
-        if redis_pool:
-            connection = redis.Redis(connection_pool=redis_pool)
-            return connection
-        logger.debug("get_redis connection_pool None")
-    except:
-        logger.debug("get_redis except")
-
-
-async def check_redis() -> bool | None:
-    try:
-        logger.debug("check_redis")
-        r: redis.Redis | None = get_redis()
-        if r:
-            return await r.ping()
-    except Exception:
-        logger.debug("check_redis fail")
-        return None
-
-redis_pool: redis.ConnectionPool = create_redis()
+async def get_db():
+    async with sessionmanager.session() as session:
+        yield session
