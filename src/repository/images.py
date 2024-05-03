4f0a5c1a702edd4886f7f831c28b7c4a7777b81b
ViKalachova <142012394+ViKalachova@users.noreply.github.com>
2024-05-03 04:34:53 +0300

    cloudinary transformation

--- src/repository/images.py
+++ src/repository/images.py
@@ -1,8 +1,12 @@
+from io import BytesIO
+
+import qrcode
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
+from starlette.responses import StreamingResponse
 
-from src.database.models import Image, User
-from src.schemas.images import ImageSchema, UpdateImageSchema
+from src.database.models import Image, User, TransformedImage
+from src.schemas.images import ImageSchema, UpdateDescriptionSchema
 from src.services.tags import get_tags_list
 
 
@@ -27,16 +31,12 @@ async def delete_image_db(db: AsyncSession, image_id: int, user: User) -> Image:
     return image
 
 
-async def update_description_db(db: AsyncSession, image_id: int, body: UpdateImageSchema, user: User) -> Image:
+async def update_description_db(db: AsyncSession, image_id: int, body: UpdateDescriptionSchema, user: User) -> Image:
     query = select(Image).filter_by(id=image_id, user_id=user.id)
     result = await db.execute(query)
     image = result.scalar_one_or_none()
     if image:
         image.description = body.description
-        # image.tags = []
-        # tags = await get_tags_list(db, body.tags)
-        # for tag in tags:
-        #     image.tags.append(tag)
         await db.commit()
         await db.refresh(image)
     return image
@@ -44,5 +44,30 @@ async def update_description_db(db: AsyncSession, image_id: int, body: UpdateIma
 
 async def get_image_db(db: AsyncSession, image_id: int, user: User):
     query = select(Image).filter_by(id=image_id, user_id=user.id)
+    result = await db.execute(query)
+    return result.scalar_one_or_none().link
+
+
+async def save_transformed_image(db: AsyncSession, link: str, image_id: int):
+    image = TransformedImage(link=link, image_id=image_id)
+    db.add(image)
+    await db.commit()
+    await db.refresh(image)
+    return image
+
+
+async def generate_qrcode_by_image(image: str):
+    qr = qrcode.QRCode(version=1, box_size=10, border=5)
+    qr.add_data(image)
+    qr.make(fit=True)
+    img = qr.make_image(fill='black', back_color='white')
+    buffer = BytesIO()
+    img.save(buffer, "PNG")
+    buffer.seek(0)
+    return StreamingResponse(buffer, media_type="image/png")
+
+
+async def get_transformed_image_db(db: AsyncSession, image_id: int):
+    query = select(TransformedImage).filter_by(id=image_id)
     result = await db.execute(query)
     return result.scalar_one_or_none().link
