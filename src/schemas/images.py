f36c01712650223df19f71ed3239487cfb3be462
ViKalachova <142012394+ViKalachova@users.noreply.github.com>
2024-05-03 12:54:27 +0300

    cloudinary transformation (#12)

--- src/schemas/images.py
+++ src/schemas/images.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Literal
 from pydantic import BaseModel, Field
 
 
@@ -7,5 +7,21 @@ class ImageSchema(BaseModel):
     tags: List[str] = Field(max_items=5)
 
 
-class UpdateImageSchema(BaseModel):
+class UpdateDescriptionSchema(BaseModel):
     description: str = Field(max_length=250)
+
+
+class UpdateImageSchema(BaseModel):
+    width: int = Field(le=1000)
+    height: int = Field(le=600)
+    crop: Literal['crop', 'scale', 'fill'] = Field(description="Choose one of the options: crop, scale, fill")
+
+
+class EffectSchema(BaseModel):
+    effect: Literal[
+        'al_dente', 'athena', 'audrey', 'aurora', 'daguerre', "eucalyptus", 'fes', "frost", 'hairspray', 'hokusai',
+        'incognito', 'linen', 'peacock', 'primavera', 'quartz', 'red_rock', 'refresh', 'sizzle', 'sonnet', 'ukulele',
+        'zorro'] = Field(
+        description="Choose one of the options: al_dente, athena, audrey, aurora, daguerre, eucalyptus, fes, frost, "
+                    "hairspray, hokusai, incognito, linen, peacock, primavera, quartz, red_rock, refresh, sizzle, "
+                    "sonnet, ukulele, zorro")
