fa520d2d0edda523b0181e536304a3b3eb43a4e3
ViKalachova <142012394+ViKalachova@users.noreply.github.com>
2024-05-03 15:21:56 +0300

    cloudinary bugfix (#14)

--- src/schemas/images.py
+++ src/schemas/images.py
@@ -14,14 +14,3 @@ class UpdateDescriptionSchema(BaseModel):
 class UpdateImageSchema(BaseModel):
     width: int = Field(le=1000)
     height: int = Field(le=600)
-    crop: Literal['crop', 'scale', 'fill'] = Field(description="Choose one of the options: crop, scale, fill")
-
-
-class EffectSchema(BaseModel):
-    effect: Literal[
-        'al_dente', 'athena', 'audrey', 'aurora', 'daguerre', "eucalyptus", 'fes', "frost", 'hairspray', 'hokusai',
-        'incognito', 'linen', 'peacock', 'primavera', 'quartz', 'red_rock', 'refresh', 'sizzle', 'sonnet', 'ukulele',
-        'zorro'] = Field(
-        description="Choose one of the options: al_dente, athena, audrey, aurora, daguerre, eucalyptus, fes, frost, "
-                    "hairspray, hokusai, incognito, linen, peacock, primavera, quartz, red_rock, refresh, sizzle, "
-                    "sonnet, ukulele, zorro")
